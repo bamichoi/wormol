9eb48f34a323a638168f1813103490d0223834c5
beomseok.choi <cbs412@naver.com>
2021-12-05 13:14:54 +0900

    touch event..

--- js/showCover.js
+++ js/showCover.js
@@ -11,31 +11,40 @@ let changeImgIsRunning = false;
 let id = null;
 
 const handleMouseOver = () => {
-    changeImgIsRunning = true;
     coverImgs[pos].style.opacity = 1;
     menu.style.opacity = 0;
 
     const changeImg = () => setInterval( () => {
-        if ( changeImgIsRunning = true){
-            coverImgs[pos].style.opacity = 0;
-            pos = (pos + 1) % imgLen;
-            coverImgs[pos].style.opacity = 1;
-        }
+        changeImgIsRunning = true;
+        coverImgs[pos].style.opacity = 0;
+        pos = (pos + 1) % imgLen;
+        coverImgs[pos].style.opacity = 1;
     }, 1500)
 
     id = changeImg();
 
     const handleMouseOut = () => {
-        changeImgIsRunning = false;
         menu.style.opacity = 1;
         coverImgs[pos].style.opacity = 0;
         clearInterval(id)
     }
 
     title.addEventListener("mouseout", handleMouseOut);
-    title.addEventListener("touchend", handleMouseOut)
+    body.addEventListener("touchstart", handleMouseOut)
     
 }
 
+const handleClick = () => {
+    if ( changeImgIsRunning == true ){
+        clearInterval(id);
+        changeImgIsRunning = false;
+    }
+    else {
+        handleMouseOver();
+    }
+
+}
+
+
 title.addEventListener("mouseover", handleMouseOver);
-title.addEventListener("touch", handleMouseOver);
+title.addEventListener("click", handleClick)
