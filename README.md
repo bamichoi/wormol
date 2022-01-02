3f6699726fa002658ced28e87d7cc4600a2ec4da
beomseok.choi <cbs412@naver.com>
2022-01-02 15:14:45 +0900

    toggle lang

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ the Homepage of music artist Wormol
 
 # ToDo List 12.12
 
-- [ ] change scale issue
-- [ ] add alt info
 - [ ] about에 사진추가
 - [ ] About 텍스트 센터정렬
 - [ ] about에 it/eg 추가
---
+++ js/toggleLang.js
@@ -0,0 +1,33 @@
+const enBtn = document.querySelector(".en_button");
+const itBtn = document.querySelector(".it_button");
+const itText = document.querySelector(".ita");
+const enText= document.querySelector(".en");
+
+let currentLang = "it";
+
+const toggleLang = (e) => {
+    const { value } = e.target;
+    console.log(value)
+    if (currentLang == value ) {
+        return
+    }
+    else {
+        if (currentLang == "it"){
+            itText.style.display = "none";
+            itBtn.style.color = "black";
+            enText.style.display = "flex";
+            enBtn.style.color ="white";
+            currentLang = "en"
+        }
+        else {
+            enText.style.display = "none";
+            enBtn.style.color ="black";
+            itText.style.display = "flex";
+            itBtn.style.color = "white";
+            currentLang = "it"
+        }
+    }
+}
+
+enBtn.addEventListener("click", toggleLang);
+itBtn.addEventListener("click", toggleLang);
