224aa50bd00d390e1b45e2a57f187791dd96c88b
beomseok.choi <cbs412@naver.com>
2022-02-12 20:09:36 +0900

    About divide in paragraphs

--- js/toggleLang.js
+++ js/toggleLang.js
@@ -1,32 +1,30 @@
 const enBtn = document.querySelector(".en_button");
 const itBtn = document.querySelector(".it_button");
 const itText = document.querySelector(".ita");
-const enText= document.querySelector(".en");
+const enText = document.querySelector(".en");
 
-let currentLang = "it";
+let currentLang = "en";
 
 const toggleLang = (e) => {
-    const { value } = e.target;
-    if (currentLang == value ) {
-        return
+  const { value } = e.target;
+  if (currentLang == value) {
+    return;
+  } else {
+    if (currentLang == "it") {
+      itText.style.display = "none";
+      itBtn.style.color = "black";
+      enText.style.display = "flex";
+      enBtn.style.color = "white";
+      currentLang = "en";
+    } else {
+      enText.style.display = "none";
+      enBtn.style.color = "black";
+      itText.style.display = "flex";
+      itBtn.style.color = "white";
+      currentLang = "it";
     }
-    else {
-        if (currentLang == "it"){
-            itText.style.display = "none";
-            itBtn.style.color = "black";
-            enText.style.display = "flex";
-            enBtn.style.color ="white";
-            currentLang = "en"
-        }
-        else {
-            enText.style.display = "none";
-            enBtn.style.color ="black";
-            itText.style.display = "flex";
-            itBtn.style.color = "white";
-            currentLang = "it"
-        }
-    }
-}
+  }
+};
 
 enBtn.addEventListener("click", toggleLang);
 itBtn.addEventListener("click", toggleLang);
